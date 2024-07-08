153a9f0ceb5c43c670165141115335a370974885
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-07-08 23:07:47 +0300

    first commit

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 // Write your code here!
 
 const mainElement = document.querySelector('main#main');
+
 document.querySelector('main#main').remove();
+
 const newHeader = document.createElement('h1');
+
 newHeader.id = 'victory';
 newHeader.textContent = 'YOUR-NAME is the champion';
+
 document.body.appendChild(newHeader);
