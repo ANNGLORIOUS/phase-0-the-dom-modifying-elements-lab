1a09bc43a63cb39853af136b89ea63c17db3c31d
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-07-08 22:51:48 +0300

    initial commit

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,10 +2,12 @@ require ( './helpers.js' );
 
 describe("index.html", () => {
   describe("after index.js is processed", () => {
+    // Test: no longer has DOM node 'main#main'
     it("no longer has DOM node 'main#main'", () => {
-      expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist
+      expect(document.querySelector('main#main'), "Make sure you remove the <main> with id 'main'").to.not.exist;
     });
 
+    // Test: has a 'newHeader' variable that points to an <h1> node
     it("has a 'newHeader' variable that points to an <h1> node", () => {
       expect(
         newHeader.nodeName,
@@ -13,6 +15,7 @@ describe("index.html", () => {
       ).eql("H1");
     });
 
+    // Test: the 'newHeader' variable that points to the <h1> node has an id of 'victory'
     it("the 'newHeader' variable that points to the <h1> node has an id of 'victory'", () => {
       expect(
         newHeader.id,
@@ -20,9 +23,10 @@ describe("index.html", () => {
       ).eql("victory");
     });
 
-    it("the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text \"YOUR-NAME is the champion\" (with your name in place of YOUR-NAME) inside it", () => {
-      expect(newHeader.innerHTML, "Make sure your <h1> with id 'victory' includes the text \"is the champion\" inside it").to.include("is the champion");
+    // Test: the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text "YOUR-NAME is the champion" inside it
+    it("the 'newHeader' variable that points to the <h1> node with an id of 'victory' has the text \"YOUR-NAME is the champion\" inside it", () => {
+      expect(newHeader.innerHTML.toLowerCase(), "Make sure your <h1> with id 'victory' includes the text \"is the champion\" inside it").to.include("is the champion".toLowerCase());
     });
 
   });
-})
+});
